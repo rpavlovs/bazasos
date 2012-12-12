6dcfefb07a1415f0eb874456f83a143a6d8f6c2c
Roman Pavlovskyi <wilohvost@gmail.com>
2012-12-12 15:19:17 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bazasos::Application.config.secret_token = '2908fe836f2d342b9e892b6d6b9a1ce513ab6ab121cb70ad8aae1c77f267e67a743923072a3fb88d4799a301a384009b73777c1e0edf593ed4cb485502bd098a'
