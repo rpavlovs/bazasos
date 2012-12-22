cb924f193168156fb58179b80631baf3c4369236
Roman Kuznietsov <roman.kuznietsov@gmail.com>
2012-12-22 14:11:05 +0200

    fix location presentation

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,17 +1,23 @@
 # encoding: utf-8
 class Location < ActiveRecord::Base
-  default_scope order('is_registration desc, is_residence desc')
+  default_scope order('(is_registration or is_residence) desc')
 
   attr_accessible :person, :person_id, :postal_code, :region, :city, :street, :street_number,
     :building_number, :floor, :entrance, :appartment, :description, :phone_number,
     :comment, :is_registration, :is_residence
 
-  validates_format_of :phone_number, with: /\d{10}/, allow_nil: true
-  validates_format_of :postal_code, with: /\d{5}/, allow_nil: true
+  validates_presence_of :description, :city, :street, :street_number
+
+  validates_format_of :phone_number, with: /\d{10}/, allow_blank: true
+  validates_format_of :postal_code, with: /\d{5}/, allow_blank: true
   validates_format_of :street_number, with: /^\d+[а-я]?(\/\d+[а-я]?)?$/
 
   belongs_to :person
 
   scope :registration, where(is_registration: true)
   scope :residence, where(is_residence: true)
+
+  def formatted_address
+    %w[region city postal_code street street_number building_number appartment entrance floor].delete_if(&:blank?).join(', ')
+  end
 end
