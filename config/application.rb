fbfee71abb7dd6f9c75f8c0685df1c46426dd16f
Roman Kuznietsov <roman.kuznietsov@gmail.com>
2012-12-25 22:21:04 +0200

    fix FactoryGirl finding factories, add some person request specs

--- config/application.rb
+++ config/application.rb
@@ -65,5 +65,9 @@ module Bazasos
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.fixture_replacement :factory_girl
+    end
   end
 end
--- spec/requests/people_spec.rb
+++ spec/requests/people_spec.rb
@@ -11,4 +11,22 @@ feature 'People' do
       page.should have_content person.cell_num
     end
   end
+
+  it 'should create new person' do
+    expect {
+      visit people_path
+      click_link I18n.t('people.index.add')
+      fill_in 'person[family_name]', with: 'Landau'
+      fill_in 'person[given_name]', with: 'Lev'
+      fill_in 'person[middle_name]', with: 'Davidovich'
+      fill_in 'person[birth_date]', with: '22.01.1908'
+      choose 'person_gender_true'
+      fill_in 'person[tax_num]', with: '1234567890'
+      fill_in 'person[cell_num]', with: '1234567890'
+      select '1 (0)', from: 'person[blood_type]'
+      choose 'person_rh_factor_true'
+      fill_in 'person[additional_details]', with: 'additional details'
+      click_button "Create #{Person.model_name.human}"
+    }.to change{ Person.count }.by(1)
+  end
 end
