0e02cfc1494d1de1dccee1303af6212bddace78f
Roman Kuznietsov <roman.kuznietsov@gmail.com>
2013-01-06 08:36:24 +0200

    add sessions request spec

---
+++ spec/requests/sessions_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+feature 'Sessions' do
+  it 'should redirect to login screen' do
+    visit people_path
+    page.should have_content I18n.t('sessions.new.title')
+  end
+
+  it 'should not redirect to login screen if logged in' do
+    log_in
+    visit people_path
+    page.should have_content I18n.t('people.index.title')
+  end
+
+  it 'should log in a user' do
+    user = FactoryGirl.create(:user)
+    visit new_session_path
+    fill_in 'credentials[login]', with: user.login
+    fill_in 'credentials[password]', with: 'password'
+    click_button I18n.t('sessions.new.login')
+    page.should have_content I18n.t('people.index.title')
+  end
+
+  it 'should not log in a user with wrong password' do
+    user = FactoryGirl.create(:user)
+    visit new_session_path
+    fill_in 'credentials[login]', with: user.login
+    fill_in 'credentials[password]', with: 'wrong password'
+    click_button I18n.t('sessions.new.login')
+    page.should have_content I18n.t('sessions.new.title')
+  end
+
+  it 'should redirect to root if logged in' do
+    log_in
+    visit new_session_path
+    page.should have_content I18n.t('people.index.title')
+  end
+
+  it 'should log out' do
+    log_in
+    visit log_out_path
+    page.should have_content I18n.t('sessions.new.title')
+  end
+end
--- spec/support/login_helper.rb
+++ spec/support/login_helper.rb
@@ -1,10 +1,9 @@
 module LoginHelper
   def log_in
-    password = 'password'
-    user = FactoryGirl.create(:user, password: password)
+    user = FactoryGirl.create(:user)
     visit new_session_path
     fill_in 'credentials[login]', with: user.login
-    fill_in 'credentials[password]', with: password
+    fill_in 'credentials[password]', with: 'password'
     click_button I18n.t('sessions.new.login')
   end
 end
